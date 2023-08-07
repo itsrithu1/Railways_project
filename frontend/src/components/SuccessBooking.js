546f4eee14515ae7a776901a86ed42f818084378
Tanmay-Nivaskar <tanmaynivaskar64980@gmail.com>
2023-08-07 22:44:33 +0530

    Payment and Add passenger Integrated

--- frontend/src/components/SuccessBooking.js
+++ frontend/src/components/SuccessBooking.js
@@ -11,63 +11,15 @@ import { Navigate ,useLocation} from 'react-router-dom';
 
 const SuccessBooking = () => {
 
-    const location = useLocation();
-  const queryParams = new URLSearchParams(location.search);
-  const passengersData = queryParams.get('passengers');
-  const passengers = JSON.parse(decodeURIComponent(passengersData));
-  const train_NumberDate = queryParams.get('train_Number');
-  const trainNumber = JSON.parse(decodeURIComponent(train_NumberDate));
+
 
     var flag =0;
     useEffect(()=>{
-       flag++;
-       console.log({flag});
-       if(flag ==1)
-       {
-        handleConfirm();
-                console.log("Im in");
-       }
+       console.log("im in useeffect");
   },[]);
 
 
-  const handleConfirm = () => {
-
-    
-    console.log(passengers);
-
-    
-        try {
-            
-            fetch(`http://localhost:3001/api/v1/passenger/addPassenger?train_Number=${trainNumber}`, {
-          method: "POST",
-          crossDomain: true,
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-            "Accesss-Control-Allow-Origin": "*",
-          },
-          body: JSON.stringify({
-            passengers
-          }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            
-            if(data.flag=="OK"){
-              alert("Seat Booked Successfully")
-            }
-    
-            });
-    
-    
-     
-        } catch (err) {
-          console.error("Error:", err);
-          
-        }
-
-
-  };
+  
 
 
     return (
