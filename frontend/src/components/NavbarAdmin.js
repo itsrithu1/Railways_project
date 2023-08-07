da778ae329fa609bf8a1df655e3dd32e30dbb102
Rithika Kalla <rithikakalla7781@gmail.com>
2023-08-07 12:23:23 +0530

    css for edit popup

---
+++ frontend/src/components/NavbarAdmin.js
@@ -0,0 +1,55 @@
+import Container from 'react-bootstrap/Container';
+
+import Nav from 'react-bootstrap/Nav';
+
+import Navbar from 'react-bootstrap/Navbar';
+
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import '../styles/Navbar.css';
+ 
+
+function NavbarAdminComponent() {
+
+  return (
+
+    <Navbar expand="lg" className="bg-body-tertiary">
+
+      <Container>
+
+        <Navbar.Brand href="#home">First Class Railways</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+
+          <Nav className="ml-auto">
+
+            {/* <Nav.Link href="/LandingPage">Modify Seach</Nav.Link> */}
+
+            {/* <Nav.Link href="#link">Destinations</Nav.Link> */}
+
+            {/* <Nav.Link href="#link">Features</Nav.Link> */}
+
+            <Nav.Link href="/">Logout </Nav.Link>
+
+          </Nav>
+
+        </Navbar.Collapse>
+
+      </Container>
+
+    </Navbar>
+
+  );
+
+}
+
+ 
+
+export default NavbarAdminComponent;
+
+
+
+
+
