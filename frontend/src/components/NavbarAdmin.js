ad77e23930e856b1172bc1a890c8ba04b57baebe
testproject5779 <testproject5579@gmail.com>
2023-08-08 16:22:22 +0530

    resolved Navbar error

--- frontend/src/components/NavbarAdmin.js
+++ frontend/src/components/NavbarAdmin.js
@@ -9,7 +9,7 @@ import {useNavigate } from "react-router-dom";
 import '../styles/Navbar.css';
 import logo from './Rail_Logo.png';
 import "../styles/Navbar.css";
-import logo from "./Rail_Logo.png";
+
  
 
 function NavbarAdminComponent() {
