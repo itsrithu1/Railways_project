93990354253d2e29cbb704dbb46a41315441e684
Rithika Kalla <rithikakalla7781@gmail.com>
2023-08-08 11:20:08 +0530

    css changes

--- frontend/src/components/TrainDetails.js
+++ frontend/src/components/TrainDetails.js
@@ -1,241 +1,195 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from 'react-bootstrap/Card';
+import Card from "react-bootstrap/Card";
 
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
-import { Button } from 'react-bootstrap';
+import { Button } from "react-bootstrap";
 
-import { Router, useNavigate ,useLocation} from 'react-router-dom';
+import { Router, useNavigate, useLocation } from "react-router-dom";
 
- 
+import "../styles/TrainDetails.css";
 
-import '../styles/TrainDetails.css';
+import NavbarComponent from "../components/NavbarComponent";
 
-import NavbarComponent from '../components/NavbarComponent';
-
-import Footer from '../components/Footer';
-
- 
+import Footer from "../components/Footer";
 
 const DisplayTrains = () => {
-  const location =useLocation()
+  const location = useLocation();
   const navigate = useNavigate();
   const [selectedTrainNumber, setSelectedTrainNumber] = useState(null);
 
   const [showMessage, setShowMessage] = useState(false);
   const handleRadioChange = (event) => {
     setSelectedTrainNumber(event.target.value);
-
   };
 
- 
-
- 
-
   const handleSubmit = () => {
-
     // window.location.href='./displaytrains'
 
-    if(selectedTrainNumber){
-      navigate(`/bookingPassengerDetails?train_Number=${selectedTrainNumber}&date=${date}`);
+    if (selectedTrainNumber) {
+      navigate(
+        `/bookingPassengerDetails?train_Number=${selectedTrainNumber}&date=${date}`
+      );
     } else {
       setShowMessage(true);
 
       setTimeout(() => {
-
         setShowMessage(false);
-
       }, 1500);
     }
-
   };
-
- 
 
   const handleRowClick = (event, trainNumber) => {
-
     setSelectedTrainNumber(trainNumber);
-
   };
 
- 
+  const [trainData, setTrainData] = useState();
 
-  const [trainData ,setTrainData]=useState()
+  const [source, setSource] = useState();
 
- 
+  const [destination, setDestination] = useState();
 
-  const [source,setSource]=useState();
+  const [date, setDate] = useState();
 
-  const [destination,setDestination]=useState();
-
-  const [date,setDate]=useState();
-
- 
-
-  const displayTrainDetails = ()=>{
-
+  const displayTrainDetails = () => {
     try {
-      fetch(`http://localhost:3001/api/v1/user/searchTrainNew?source=${source}&destination=${destination}&date=${date}`, {
-    method: "GET",
-    crossDomain: true,
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-      "Accesss-Control-Allow-Origin": "*",
-    },
-
-  })
-
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("recieved data is", data.data)
-      if (data.flag=="OK") {
-        setTrainData(data.data)
-            } else {
-              // alert("No Trains Found")
-            }
-      });
-  } catch (err) {
-    console.error("Error:", err);
-  }
+      fetch(
+        `http://localhost:3001/api/v1/user/searchTrainNew?source=${source}&destination=${destination}&date=${date}`,
+        {
+          method: "GET",
+          crossDomain: true,
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "Accesss-Control-Allow-Origin": "*",
+          },
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("recieved data is", data.data);
+          if (data.flag == "OK") {
+            setTrainData(data.data);
+          } else {
+            // alert("No Trains Found")
+          }
+        });
+    } catch (err) {
+      console.error("Error:", err);
+    }
     // console.log(source,destination,date)
-  }
+  };
 
   useEffect(() => {
-
-    const queryParams= new URLSearchParams(location.search);
-    setSource( queryParams.get("source"))
-    setDestination( queryParams.get("destination"))
+    const queryParams = new URLSearchParams(location.search);
+    setSource(queryParams.get("source"));
+    setDestination(queryParams.get("destination"));
     const parts = queryParams.get("date").split("-");
     const formattedDate = `${parts[2]}-${parts[1]}-${parts[0]}`;
-    setDate(formattedDate)
+    setDate(formattedDate);
     displayTrainDetails();
-
   }, [date]);
 
- 
-
   return (
-
-   
-
     <>
+      <NavbarComponent />
 
-    <NavbarComponent/>
+      <div className="page-container">
+        <div
+          className="content-box train-details-container"
+          style={{ color: "black" }}
+        >
+          <h2>Train Details</h2>
 
-    <div className='page-container'>
+          <p>
+            <b>Source: {source}</b>
+          </p>
 
- 
+          <p>
+            <b>Destination: {destination}</b>
+          </p>
 
-      <div className="content-box train-details-container" style={{color:'black'}}> 
+          <p>
+            <b>Date: {date}</b>
+          </p>
 
-      <h2>Train Details</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Book This?</th>
 
-      <p><b>Source: {source}</b></p>
+                <th>Train No.</th>
 
-      <p><b>Destination: {destination}</b></p>
+                <th>Train Name</th>
 
-      <p><b>Date: {date}</b></p>
+                <th>Departure Time</th>
 
-      <table>
+                <th>Arrival Time</th>
 
-        <thead>
+                <th>Fare</th>
 
-          <tr>
+                <th>Seats Available</th>
+              </tr>
+            </thead>
 
-            <th>Book This?</th>
+            <tbody>
+              {trainData &&
+                trainData.map((train, index) => (
+                  <tr
+                    key={index}
+                    onClick={(event) =>
+                      handleRowClick(event, train.train_Number)
+                    }
+                  >
+                    <td>
+                      <Form.Check
+                        type="radio"
+                        name="TrainBooking"
+                        id={`Train${index + 1}`}
+                        value={train.train_Number}
+                        checked={selectedTrainNumber === train.train_Number}
+                        onChange={handleRadioChange}
+                        required
+                      />
+                    </td>
 
-            <th>Train No.</th>
+                    <td>{train.train_Number}</td>
 
-            <th>Train Name</th>
+                    <td>{train.name}</td>
 
-            <th>Departure Time</th>
+                    <td>{train.departure_time}</td>
 
-            <th>Arrival Time</th>
+                    <td>{train.arrival_time}</td>
 
-            <th>Fare</th>
+                    <td>{train.fare}</td>
 
-            <th>Seats Available</th>
+                    <td>{train.totalSeatsAvailable}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
 
-          </tr>
+          <div style={{ opacity: "100%" }}>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={handleSubmit}
+              style={{ opacity: "100%" }}
+            >
+              Submit
+            </Button>
+          </div>
 
-        </thead>
-
-        <tbody>
-
- 
-
-          {trainData && trainData.map((train, index) => (
-
-            <tr key={index}
-
-            onClick={(event) => handleRowClick(event, train.train_Number)}>
-
-              <td>
-
-              <Form.Check
-
-                  type="radio"
-
-                  name="TrainBooking"
-
-                  id={`Train${index + 1}`}
-
-                  value={train.train_Number}
-
-                  checked={selectedTrainNumber === train.train_Number}
-
-                  onChange={handleRadioChange}
-                  required
-
-                />
-
-              </td>
-
-              <td>{train.train_Number}</td>
-
-              <td>{train.name}</td>
-
-              <td>{train.departure_time}</td>
-
-              <td>{train.arrival_time}</td>
-
-              <td>{train.fare}</td>
-
-              <td>{train.totalSeatsAvailable}</td>
-
-            </tr>
-
-          ))}
-
-        </tbody>
-
-      </table>
-
- 
-<div style={{opacity:'100%'}}>
-      <Button variant="primary" type="submit" onClick={handleSubmit} style={{opacity:'100%'}}  >
-
-        Submit
-
-      </Button>
+          {showMessage && (
+            <p style={{ color: "red" }}>Please select a train.</p>
+          )}
+        </div>
       </div>
 
-      {showMessage && <p style={{ color: 'red' }}>Please select a train.</p>}
-
-    </div>
-
-    </div>
-
-    <Footer/>
-
+      <Footer />
     </>
-
   );
-
 };
-
- 
 
 export default DisplayTrains;
